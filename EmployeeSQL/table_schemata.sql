46a41e6a1eae0f468488b1c97d345be892e06cba
Geraldine <galongo@uci.edu>
2023-01-24 12:08:09 -0800

    finalized table schemata

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,35 +1,33 @@
---drop all tables to create if they alreasy exist
-DROP TABLE IF EXISTS departments CASCADE;
-DROP TABLE IF EXISTS dept_emp CASCADE;
-DROP TABLE IF EXISTS dept_manager CASCADE;
-DROP TABLE IF EXISTS employees CASCADE;
-DROP TABLE IF EXISTS salaries CASCADE;
+--drop titles table
 DROP TABLE IF EXISTS titles CASCADE;
-
 --create the titles table
 CREATE TABLE titles (
-	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
+	title_id VARCHAR(10) NOT NULL,
+	title VARCHAR(30) NOT NULL,
 	--set the title id as a primary key
 	PRIMARY KEY(title_id)
 );
 
+--drop departments table
+DROP TABLE IF EXISTS departments CASCADE;
 --create the departments table
 CREATE TABLE departments (
-	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	dept_name VARCHAR(30) NOT NULL,
 	--set the department number as a primary key
 	PRIMARY KEY(dept_no)
 );
 
+--drop employees table
+DROP TABLE IF EXISTS employees CASCADE;
 --create the employees table
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(5) NOT NULL,
 	hire_date DATE NOT NULL,
 	--set the employee number as primary key
 	PRIMARY KEY(emp_no),
@@ -37,9 +35,11 @@ CREATE TABLE employees (
 	FOREIGN KEY(emp_title_id) REFERENCES titles (title_id)
 );
 
+--drop dept_manager table
+DROP TABLE IF EXISTS dept_manager CASCADE;
 --create the department manager table
 CREATE TABLE dept_manager (
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	emp_no INT NOT NULL,
 	--set department number as foreign key with department number from departments table as reference
 	FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
@@ -47,16 +47,20 @@ CREATE TABLE dept_manager (
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
 );
 
+--drop dept_emp table
+DROP TABLE IF EXISTS dept_emp CASCADE;
 --create the department employee table
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	--set employee number as foreign key with employee number from employees table as reference
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
 	--set department number as foreign key with department number from departments table as reference
 	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
 );
 
+--drop salaries table
+DROP TABLE IF EXISTS salaries CASCADE;
 --create the salaries table
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
